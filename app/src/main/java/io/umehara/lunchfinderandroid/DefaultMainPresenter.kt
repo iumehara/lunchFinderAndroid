564bc986e2c42f1e4f1fa8f9e85854636b7e3631
iumehara <iumehara10@gmail.com>
2018-07-27 12:11:04 +0900

    Adds Retrofit for requests to server

--- app/src/main/java/io/umehara/lunchfinderandroid/DefaultMainPresenter.kt
+++ app/src/main/java/io/umehara/lunchfinderandroid/DefaultMainPresenter.kt
@@ -1,16 +1,52 @@
 package io.umehara.lunchfinderandroid
 
-import java.util.Arrays.asList
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 import javax.inject.Inject
 
 class DefaultMainPresenter @Inject constructor(private val mainView: MainView): MainPresenter {
     override fun getRestaurants() {
-        val restaurants = asList(
-                Restaurant(1, "First", "一"),
-                Restaurant(2, "Second", "二"),
-                Restaurant(3, "Third", "三")
-        )
+        val defaultRestaurantRepo = DefaultRestaurantRepo()
+        val callback = defaultRestaurantRepo.getAll()
 
-        mainView.setRow(restaurants)
+        callback.enqueue(object: Callback<List<Restaurant>> {
+            override fun onFailure(call: Call<List<Restaurant>>?, t: Throwable?) {
+                if (t != null) { println("getRestaurant request failed:" + t.message.toString()) }
+            }
+
+            override fun onResponse(call: Call<List<Restaurant>>?, response: Response<List<Restaurant>>?) {
+                if (response == null) { println("getRestaurant request received no response"); return }
+                if (!response.isSuccessful) { println("getRestaurant request received unsuccessful response: " + response.message()); return }
+
+                val restaurants = response.body() ?: return
+
+                mainView.setRow(restaurants)
+            }
+        })
     }
+}
+
+class DefaultRestaurantRepo: RetrofitRestaurantCaller {
+    private var retrofitRestaurantCaller: RetrofitRestaurantCaller
+
+    init {
+        val retrofit = Retrofit.Builder()
+                .baseUrl("http://localhost:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        retrofitRestaurantCaller = retrofit.create(RetrofitRestaurantCaller::class.java)
+    }
+
+    override fun getAll(): Call<List<Restaurant>> {
+        return retrofitRestaurantCaller.getAll()
+    }
+}
+
+interface RetrofitRestaurantCaller {
+    @GET("restaurants")
+    fun getAll(): Call<List<Restaurant>>
 }
