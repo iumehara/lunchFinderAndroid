fc3525ecc146cd695a86f236067af5f18f78a3fa
iumehara <iumehara10@gmail.com>
2018-07-30 16:42:27 +0900

    Adds MapView to MainActivity

--- app/src/main/java/io/umehara/lunchfinderandroid/MainActivity.kt
+++ app/src/main/java/io/umehara/lunchfinderandroid/MainActivity.kt
@@ -1,23 +1,39 @@
 package io.umehara.lunchfinderandroid
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import dagger.android.support.DaggerAppCompatActivity
 import io.umehara.lunchfinderandroid.restaurant.Restaurant
 import javax.inject.Inject
 
-class MainActivity: DaggerAppCompatActivity(), MainView {
+
+class MainActivity: DaggerAppCompatActivity(), MainView, OnMapReadyCallback {
+    private lateinit var map: MapView
+
     @Inject
     lateinit var presenter: MainPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         presenter.getRestaurants()
+
+        map = findViewById(R.id.multiple_marker_map)
+
+        map.onCreate(savedInstanceState)
+        map.getMapAsync(this)
     }
 
     override fun setRow(restaurants: List<Restaurant>) {
@@ -34,8 +50,50 @@ class MainActivity: DaggerAppCompatActivity(), MainView {
         restaurantList.onItemClickListener = clickListener
     }
 
+    override fun onMapReady(map: GoogleMap) {
+        val startingPoint = LatLng(35.660480, 139.729247)
+        val startingMarker = MarkerOptions().position(startingPoint).title("Roppongi Hills")
+        map.addMarker(startingMarker)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(startingPoint, 15.0F))
+    }
+
     override fun setDetail(restaurant: Restaurant) {
         val restaurantNameLabel = findViewById<TextView>(R.id.restaurant_name)
         restaurantNameLabel.text = restaurant.name
+    }
+
+    override fun onStart() {
+        super.onStart()
+        map.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        map.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        map.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        map.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        map.onLowMemory()
     }
 }
